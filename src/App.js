9f9d957c124fe0dd8fac990143b305f495345d00
Tsotne91 <tsotne91@gmail.com>
2022-01-22 18:20:10 +0400

    Finished Card Component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import {Col, Row} from "react-bootstrap";
 import MyCard from "./MyCard";
 import axios from "axios";
 import React, {useEffect, useState} from "react";
+import GridModal from './GridModal'
 
 function App() {
     const [albums, setAlbums] = useState([]);
+    const [modalShow, setModalShow] = useState(false);
 
     useEffect(()=>{
         async function getAlbums() {
@@ -20,7 +22,12 @@ function App() {
           <Row className="m-5">
               {albums.map((album) => (
                   <Col key={album.id} md={3} sm={6} className="my-2">
-                      <MyCard title={album.title} albumId={album.id} userId={album.userId}/>
+                      <MyCard title={album.title}
+                              albumId={album.id}
+                              userId={album.userId}
+                              onClick={()=>setModalShow(true)} //not working
+                      />
+                      <GridModal onHide={() => setModalShow(false)}/>
                   </Col>
               ))}
           </Row>
--- src/MyCard.js
+++ src/MyCard.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from 'react-bootstrap/Card';
-// import AlbumCoversModal from "./AlbumCoversModal";
-
+import GridModal from './GridModal'
 
 export default function MyCard ({title, albumId, userId}){
 
-    return <Card className="h-100 bg-info fs-4" role="button">
-        <p className="m-2 text-light">ალბომი {albumId}</p>
-        <p className="m-2 text-light">userId {userId}</p>
-        <div className="border-primary text-secondary m-2">
+    return <Card className="h-100 bg-info fs-4" role="button"  >
+        <Card.Header>
+        <p className="m-1 text-light">ალბომი {albumId}</p>
+        </Card.Header>
+        <p className="mx-4 m-1 text-light">userId {userId}</p>
+        <div className="border-primary text-secondary mx-4 m-1">
             <div className="w-100">{title}</div>
         </div>
     </Card>
